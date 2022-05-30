2bae283405c31c77c795702b286b1e1b79b54ede
ayminahmedpk <ayminahmedpk@gmail.com>
2022-05-30 14:49:29 +0500

    0.0.7

---
+++ src/01-js-fundamentals-and-this/02-objects/03-objects-accessors/objects-accessors.test.js
@@ -0,0 +1,51 @@
+const simpleAccessors = {
+  name    : "John",
+  surname : "Smith",
+  get fullName() { return `${this.name} ${this.surname}`; },
+  set fullName(fullName) {[this.name, this.surname] = fullName.split(" ");}
+}
+
+const smartAccessors = {
+  get name() {return this._name;} ,
+  set name(name) {
+    if(name.length < 4) {
+      console.log('Name is too short; minimum 4 characters');
+      return;
+    }
+    this._name = name;
+  },
+}
+
+describe('Describe block to nest other describe blocks, for sequential testing', () => {
+
+  describe('', () => { test('Using a simple accessor to read', () => {
+    expect(simpleAccessors.fullName).toBe('John Smith');
+  });});
+
+  describe('',() => {
+    beforeAll(() => { simpleAccessors.fullName = "John Doe"; })
+    test('Using a simple accessor to write', () => {
+      expect([simpleAccessors.name, simpleAccessors.surname]).
+      toEqual(['John', 'Doe']);
+    });
+  });
+
+  describe('', () => { test('Using a smart accessor to read nonexistent property', () => {
+    expect(smartAccessors.name).toBeUndefined();
+  });});
+
+  describe('', () => {
+    beforeAll(() => {smartAccessors.name = 'Michael';});
+    test('Checking if smart accessor set the right property', () => {
+      expect(smartAccessors._name).toBe('Michael');
+    });
+  });
+
+  describe('', () => {
+    beforeAll(() => {smartAccessors.name = 'Joe';});
+    test('Checking if smart accessor blocked invalid write attempt', () => {
+      expect((smartAccessors._name)).toBe('Michael');
+    });
+  });
+
+});
